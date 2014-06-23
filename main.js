6adeacecc8c3d629e9dbb44794354e7e5478c835
lc00 <leannecheung00@gmail.com>
2014-06-23 14:38:20 -0600

    completed with this exercise

--- main.js
+++ main.js
@@ -1,4 +1,24 @@
 var userInput = prompt("Enter anything");
-alert(userInput);
-alert(userInput.length);
-// alert();
+document.write(
+
+	 "You entered: " + userInput + '<br>' +
+	 "There are " + userInput.length + " in the word" + '<br>' +
+	 'The third character is ' + "\'" + userInput[2] + "\'"+ '<br>' +
+	 'Lowercase: ' + userInput.toLowerCase() + '<br>' +
+	'Uppercase: ' + userInput.toUpperCase() + '<br>' +
+	'Example: What is going on in ' + userInput + '<br>' +
+	'Subword: ' + userInput.substring(1,[4])
+
+	);
+
+
+// alert(userInput);
+
+
+
+// alert(userInput.length);
+// alert(userInput.charAt(2));
+// alert(userInput.toLowerCase());
+// alert(userInput.toUpperCase());
+// alert("What is hot in " + userInput);
+// alert(userInput.substring(1,[4]));
